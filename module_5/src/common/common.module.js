6c061458c2e6f9bff8896a3f0955ac2f92002308
SarthakYelne <sarthakyelne2003@gmail.com>
2024-05-02 20:47:58 +0530

    interceptors

--- module_5/src/common/common.module.js
+++ module_5/src/common/common.module.js
@@ -1,6 +1,12 @@
 (function() {
 "use strict";
 
-angular.module('common', []);
+angular.module('common', [])
+.config(config);
+
+config.$inject = ['$httpProvider'];
+function config($httpProvider) {
+    $httpProvider.loadingIntercepters.push('loadingHttpIntercepter');
+}
 
 })();
