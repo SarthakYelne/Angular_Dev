c773bd01d3798d4387b95a33547172b71e6004ce
SarthakYelne <sarthakyelne2003@gmail.com>
2024-05-02 14:25:00 +0530

    module_4

---
+++ module_4/src/menu/data/menudata.service.js
@@ -0,0 +1,30 @@
+(function () {
+  'use strict';
+
+  angular.module('data')
+  .service('MenuDataService', MenuDataService)
+  .constant('apiBasePath', 'https://coursera-jhu-default-rtdb.firebaseio.com/');
+
+  MenuDataService.$inject = ['$http', 'apiBasePath'];
+  function MenuDataService ($http, apiBasePath) {
+    var service = this;
+
+    service.getAllCategories = function () {
+      var result = $http({
+        method: 'GET',
+        url: (apiBasePath + 'categories.json')
+      });
+
+      return result;
+    }
+
+    
+    service.getItemsForCategory = function (Name) {
+      return $http({
+        method: 'GET',
+        url: (apiBasePath + 'menu_items/'+Name+'.json') 
+      });
+    }
+  }
+
+})();
